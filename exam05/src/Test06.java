4a9a6f29edf4f7b89928ba0a79041c81128d4be0
ShrimpApple <how1130@naver.com>
2024-09-30 17:54:07 +0900

    자바 코딩 연습 기초 5

--- exam05/src/Test06.java
+++ exam05/src/Test06.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //왼쪽 위가 직각인 이등변 삼각형
 import java.util.Scanner;
 
@@ -16,3 +17,23 @@ public class Test06 {
         }
     }
 }
+=======
+//왼쪽 위가 직각인 이등변 삼각형
+import java.util.Scanner;
+
+public class Test06 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("왼쪽 위가 직각인 이등변 삼각형을 표시합니다.");
+        System.out.println("단수는?:");
+        int n = stdIn.nextInt();
+
+        for (int i=1; i <= n; i++) {
+            for (int j=1; j <= n-i+1; j++)
+                System.out.print('*');
+            System.out.println();
+        }
+    }
+}
+>>>>>>> 011ade6a75d62de0c1dbdd640f4a2e97a78227b2
